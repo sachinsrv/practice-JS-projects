a40560a7b5972ce4f1d681450f01edff03c200a1
SACHIN SAURAV <schn.srv@gmail.com>
2021-09-19 01:59:04 +0530

    added flipper logic and minor css changes

--- Color Flipper/app.js
+++ Color Flipper/app.js
@@ -1,10 +1,16 @@
-const colors = ["red", "blue", "violet", "green"];
+const colors = ["red", "blue", "violet", "green", "yellow"];
 
-const btn = document.getElementById('btn');
-const color = document.querySelector('.color');
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
-btn.addEventListener("click", function(){
-    const randomColor = 2;
-    document.querySelector('body').style.backgroundColor = colors[randomColor];
-    color.textContent = colors[randomColor];
+btn.addEventListener("click", function () {
+  //const randomColor = 2;
+  getRandomColor();
 });
+
+function getRandomColor() {
+  let randomColor = Math.floor(Math.random() * colors.length);
+  console.log(randomColor);
+  document.querySelector("body").style.backgroundColor = colors[randomColor];
+  color.textContent = colors[randomColor];
+}
